84eb4923aa675493e80bfbd2a1270d16beac8b01
thepigcat <benpospo@gmail.com>
2024-02-09 15:30:16 +0100

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -11,35 +11,53 @@ const kwm_renderer = new KwmRenderer()
   .setupCamera(new three.Vector3(0, 2, 18))
   .setupLights();
 
-const kwm_handler = new KeyHandler();
+const carbon1 = kwm_renderer.renderAtom(
+  {
+    atom_type: AtomType.Chlorine,
+    connections: Array.of(),
+    charge: 0,
+  },
+  new three.Vector3(0, 5, 2)
+);
 
-const carbon1 = kwm_renderer.renderAtom({
-  atom_type: AtomType.Chlorine,
-  connections: Array.of(),
-  charge: 0,
-}, new three.Vector3(0, 5, 2));
+const carbon2 = kwm_renderer.renderAtom(
+  {
+    atom_type: AtomType.Hydrogen,
+    connections: Array.of(),
+    charge: 0,
+  },
+  new three.Vector3(0, 0, -10)
+);
 
-const carbon2 = kwm_renderer.renderAtom({
-  atom_type: AtomType.Hydrogen,
-  connections: Array.of(),
-  charge: 0,
-}, new three.Vector3(0, 0, -10));
-
-const carbon3 = kwm_renderer.renderAtom({
-  atom_type: AtomType.Nitrogen,
-  charge: 0,
-  connections: Array.of()
-}, new three.Vector3(0, -6, 3));
-
-kwm_handler.createKeyMap(new Map()
-  .set(Key.)
+const carbon3 = kwm_renderer.renderAtom(
+  {
+    atom_type: AtomType.Nitrogen,
+    charge: 0,
+    connections: Array.of(),
+  },
+  new three.Vector3(0, -6, 3)
 );
 
 kwm_renderer.onUpdate = () => {
-  carbon1.rotateX(0.02).rotateY(0.01).rotateZ(0.02);
-  carbon2.rotateX(0.02).rotateY(0.01).rotateZ(0.01);
+  // This function gets called every frame
 };
 
 kwm_renderer.animate();
 
-document.addEventListener("keydown", kwm_handler.handleKeyPress);
+const kwm_handler = new KeyHandler()
+  .createKeyMap(
+    new Map()
+      .set([Key.Space1, Key.Space2, Key.Up], () => {
+        carbon3.position.y += 0.3;
+      })
+      .set([Key.Shift, Key.Down], () => {
+        carbon3.position.y -= 0.3;
+      })
+      .set([Key.Right], () => {
+        carbon3.position.x += 0.3;
+      })
+      .set([Key.Left], () => {
+        carbon3.position.x -= 0.3;
+      })
+  )
+  .setupKeyListeners();
--- src/modules/keybinds.ts
+++ src/modules/keybinds.ts
@@ -1,23 +1,63 @@
 export enum Key {
-    Up = "ArrowUp",
-    Down = "ArrowDown",
-    Left = "ArrowLeft",
-    Right = "ArrowRight",
+  Up = "ArrowUp",
+  Down = "ArrowDown",
+  Left = "ArrowLeft",
+  Right = "ArrowRight",
+  Space1 = "Space",
+  Space2 = " ",
+  Shift = "Shift",
 }
 
-export class KeyHandler {
-  private keys: Map<string, () => void> 
-  
-  constructor() {
-    this.keys = new Map();
-  }
+type Keymap = Map<Array<string>, () => void>;
 
-  createKeyMap(keys: Map<string, () => void>): this {
-    this.keys = keys;
+export class KeyHandler {
+  constructor(
+    private keymap: Keymap = new Map(),
+    private pressedKeys: Array<string> = []
+  ) {}
+
+  // Setup functions
+  createKeyMap(keymap: Keymap): this {
+    this.keymap = keymap;
     return this;
   }
 
-  handleKeyPress(event: KeyboardEvent): void {
-    this.keys.get(event.key)!()
+  setupKeyListeners(): this {
+    document.addEventListener("keydown", (event) => this.handleKeyDown(event));
+    document.addEventListener("keyup", (event) => this.handleKeyUp(event));
+    return this;
+  }
+
+  // Helper functions for key events
+  handleKeyDown(event: KeyboardEvent) {
+    if (!this.pressedKeys.includes(event.key)) {
+      this.pressedKeys.push(event.key);
+    }
+
+    this.checkKeys();
+  }
+
+  handleKeyUp(event: KeyboardEvent) {
+    // Remove the released key from the array
+    const index = this.pressedKeys.indexOf(event.key);
+    if (index !== -1) {
+      this.pressedKeys.splice(index, 1);
+    }
+
+    // Check if multiple keys are still being held down
+    this.checkKeys();
+  }
+
+  checkKeys(this: KeyHandler) {
+    for (let j = 0; j < this.keymap.size; j++) {
+      const [keys, action] = Array.from(this.keymap.entries())[j];
+      for (let k = 0; k < keys.length; k++) {
+        if (this.pressedKeys.includes(keys[k])) {
+          action();
+        }
+      }
+    }
+
+    console.log("Keys: " + this.keymap);
   }
 }
